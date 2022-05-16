40bf287afe5431036fe44a7e3601a31ed4a5b880
D. van Braak <bkd@hoornbeeck.nl>
2022-05-16 22:00:52 +0200

    Update verwerken.php
    Hashing er even tussenuit gehaald als voorbeeld laten gebruiken door student...

--- verwerken.php
+++ verwerken.php
@@ -4,9 +4,9 @@ include_once 'conn/database.php';
 global $dbconn;
 //afvangen formuliervelden...
 if (isset($_POST['submit'])){
-    //opdracht 4 - SQL-injection: mysqli_real_escape_string()
-    $inlognaam = mysqli_real_escape_string($dbconn, $_POST['inlognaam']);
-    $wachtwoord = mysqli_real_escape_string($dbconn, $_POST['wachtwoord']);
+    $inlognaam = $_POST['inlognaam'];
+    $wachtwoord = $_POST['wachtwoord'];
+    $_SESSION['ingelogd']=true;
 }
 else {
     //niet gepost...
@@ -17,25 +17,15 @@ else {
 $query = "SELECT id, gebruikersnaam, wachtwoord FROM personeel
             WHERE gebruikersnaam='$inlognaam' and wachtwoord='$wachtwoord';";
 $result= mysqli_query($dbconn, $query);
+
 //aantal records bepalen mysqli_num_rows($result);
 $aantal = mysqli_num_rows($result);
-
+echo 'AANTAL: ' .  $aantal . '<br>';
 
 //aantal=1 =>Programma.php...
 if ($aantal==1) {
-    $user=mysqli_fetch_array($result);
-    // extra toevoegen: password_verify waarbij je het ingegeven ww vergelijkt met dat wat in de database staat
-    if (password_verify($wachtwoord, $user["wachtwoord"])) {
-        $_SESSION['ingelogd']=true;
-        header('refresh: 1; programma.php');
-        exit;
-    }
-    else {
-        $_SESSION['ingelogd']=false;
-        header('refresh: 1; login.php');
-        exit;
-    }
-
+    header('refresh: 1; _programma.php');
+    exit;
 }
 else {
     header('refresh: 1; login.php');
